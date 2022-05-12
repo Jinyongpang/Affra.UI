e50167557b994f862d44a80ae5654f197836543e
Jianyi Lim <jianyi.lim@appspace.com>
2022-05-12 22:21:58 +0800

    Fix location.

--- src/JXNippon.CentralizedDatabaseSystem/Pages/PowerGenerationAndDistributionManagement.razor.cs
+++ src/JXNippon.CentralizedDatabaseSystem/Pages/PowerGenerationAndDistributionManagement.razor.cs
@@ -7,20 +7,16 @@ namespace JXNippon.CentralizedDatabaseSystem.Pages
     public partial class PowerGenerationAndDistributionManagement
     {
         private PowerGenerationAndDistributionManagementDataGrid dataGrid;
-        private DeOilerInjectionDataGrid deOilerInjectionDataGrid;
         private PowerGenerationAndDistributionManagementFilterPanel filterPanel;
 
         private async Task LoadDataAsync(LoadDataArgs args)
         {
             dataGrid.CommonFilter = filterPanel.CommonFilter;
-            deOilerInjectionDataGrid.CommonFilter = filterPanel.CommonFilter;
         }
 
         private async Task OnChangeAsync(CommonFilter commonFilter)
         {
-            await Task.WhenAll( dataGrid.ReloadAsync(),
-                deOilerInjectionDataGrid.ReloadAsync());
-
+            await dataGrid.ReloadAsync();
         }
     }
 }
