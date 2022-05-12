84687173fa0764efa97e6cb28562ce0e35c12e52
Jianyi Lim <jianyi.lim@appspace.com>
2022-05-12 16:59:16 +0800

    Standardization and alignment fix.

--- src/JXNippon.CentralizedDatabaseSystem/Shared/ProducedWaterTreatmentSystemManagementDataGrid.razor.cs
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/ProducedWaterTreatmentSystemManagementDataGrid.razor.cs
@@ -19,6 +19,7 @@ namespace JXNippon.CentralizedDatabaseSystem.Shared
         [Parameter] public EventCallback<LoadDataArgs> LoadData { get; set; }
         [Parameter] public bool ShowRefreshButton { get; set; }
         [Parameter] public bool PagerAlwaysVisible { get; set; }
+        [Parameter] public bool ShowDateColumn { get; set; }
         [Inject] private IServiceProvider ServiceProvider { get; set; }
         [Inject] private NotificationService NotificationService { get; set; }
         public CommonFilter CommonFilter { get; set; }
