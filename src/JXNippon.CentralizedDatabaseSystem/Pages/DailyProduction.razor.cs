a35e2bd4b6cf99418c4429585073529963eb921e
Duncan Foo <Ev_yi6146@hotmail.com>
2022-05-13 16:53:34 +0800

    Added Daily Production Sections

---
+++ src/JXNippon.CentralizedDatabaseSystem/Pages/DailyProduction.razor.cs
@@ -0,0 +1,36 @@
+﻿using JXNippon.CentralizedDatabaseSystem.Models;
+using JXNippon.CentralizedDatabaseSystem.Shared;
+using Radzen;
+
+
+namespace JXNippon.CentralizedDatabaseSystem.Pages
+{
+    public partial class DailyProduction
+    {
+        private ProductionSK10DataGrid sk10DataGrid;
+        private ProductionHIPDataGrid hipDataGrid;
+        private ProductionFPSOHelangDataGrid fpsoDataGrid;
+
+        private DailyProductionFilterPanel filterPanel;
+
+        private async Task LoadDataAsync(LoadDataArgs args)
+        {
+            sk10DataGrid.CommonFilter = filterPanel.CommonFilter;
+            hipDataGrid.CommonFilter = filterPanel.CommonFilter;
+            fpsoDataGrid.CommonFilter = filterPanel.CommonFilter;
+        }
+
+        private async Task OnChangeAsync(CommonFilter commonFilter)
+        {
+            await Task.WhenAll(sk10DataGrid.ReloadAsync(),
+                hipDataGrid.ReloadAsync(),
+                fpsoDataGrid.ReloadAsync());
+        }
+        private async Task ReloadAsync()
+        {
+            await Task.WhenAll(sk10DataGrid.ReloadAsync(),
+                hipDataGrid.ReloadAsync(),
+                fpsoDataGrid.ReloadAsync());
+        }
+    }
+}
---
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/DailyProductionFilterPanel.razor.cs
@@ -0,0 +1,34 @@
+﻿using Affra.Core.Domain.Extensions;
+using Affra.Core.Domain.Services;
+using CentralizedDatabaseSystemODataService.Affra.Service.CentralizedDatabaseSystem.Domain.DailyProductions;
+using DataExtractorODataService.Affra.Service.DataExtractor.Domain.DataFiles;
+using JXNippon.CentralizedDatabaseSystem.Domain.CentralizedDatabaseSystemServices;
+using JXNippon.CentralizedDatabaseSystem.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace JXNippon.CentralizedDatabaseSystem.Shared
+{
+    public partial class DailyProductionFilterPanel
+    {
+        private IEnumerable<SK10ProductionItem> datas;
+
+        [Parameter] public EventCallback<CommonFilter> Change { get; set; }
+        [Inject] private NavigationManager NavManager { get; set; }
+        [Inject] private IServiceProvider ServiceProvider { get; set; }
+        public CommonFilter CommonFilter { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            CommonFilter = new CommonFilter(NavManager);
+
+            using var serviceScope = ServiceProvider.CreateScope();
+            datas = (await serviceScope.ServiceProvider.GetRequiredService<IUnitGenericService<SK10ProductionItem, ICentralizedDatabaseSystemUnitOfWork>>()
+                .Get()
+                .ToQueryOperationResponseAsync<SK10ProductionItem>()).ToList();
+        }
+        private async Task OnChangeAsync(object value)
+        {
+            CommonFilter.AppendQuery(NavManager);
+            await Change.InvokeAsync(CommonFilter);
+        }
+    }
+}
