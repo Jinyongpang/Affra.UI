2e610d0b5cffa07835150e2dabb98132491df355
Jianyi Lim <jianyi.lim@appspace.com>
2022-05-14 00:31:16 +0800

    Use NameFilter for common name filter.

--- src/JXNippon.CentralizedDatabaseSystem/Shared/DailyProductionFilterPanel.razor.cs
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/DailyProductionFilterPanel.razor.cs
@@ -1,7 +1,6 @@
 ﻿using Affra.Core.Domain.Extensions;
 using Affra.Core.Domain.Services;
 using CentralizedDatabaseSystemODataService.Affra.Service.CentralizedDatabaseSystem.Domain.DailyProductions;
-using DataExtractorODataService.Affra.Service.DataExtractor.Domain.DataFiles;
 using JXNippon.CentralizedDatabaseSystem.Domain.CentralizedDatabaseSystemServices;
 using JXNippon.CentralizedDatabaseSystem.Models;
 using Microsoft.AspNetCore.Components;
@@ -10,7 +9,7 @@ namespace JXNippon.CentralizedDatabaseSystem.Shared
 {
     public partial class DailyProductionFilterPanel
     {
-        private IEnumerable<SK10ProductionItem> datas;
+        private IEnumerable<NameFilter> datas;
 
         [Parameter] public EventCallback<CommonFilter> Change { get; set; }
         [Inject] private NavigationManager NavManager { get; set; }
@@ -21,9 +20,32 @@ namespace JXNippon.CentralizedDatabaseSystem.Shared
             CommonFilter = new CommonFilter(NavManager);
 
             using var serviceScope = ServiceProvider.CreateScope();
-            datas = (await serviceScope.ServiceProvider.GetRequiredService<IUnitGenericService<SK10ProductionItem, ICentralizedDatabaseSystemUnitOfWork>>()
+            var sk = serviceScope.ServiceProvider.GetRequiredService<IUnitGenericService<SK10ProductionItem, ICentralizedDatabaseSystemUnitOfWork>>()
                 .Get()
-                .ToQueryOperationResponseAsync<SK10ProductionItem>()).ToList();
+                .ToQueryOperationResponseAsync<SK10ProductionItem>();
+            var hip = serviceScope.ServiceProvider.GetRequiredService<IUnitGenericService<HIPProductionItem, ICentralizedDatabaseSystemUnitOfWork>>()
+                .Get()
+                .ToQueryOperationResponseAsync<HIPProductionItem>();
+            var fpso = serviceScope.ServiceProvider.GetRequiredService<IUnitGenericService<FPSOHelangProductionItem, ICentralizedDatabaseSystemUnitOfWork>>()
+                .Get()
+                .ToQueryOperationResponseAsync<FPSOHelangProductionItem>();
+
+            List<string> names = (await sk)
+                .Select(item => item.Name)
+                .ToList();
+
+            names.AddRange((await hip)
+                .Select(item => item.Name)
+                .ToList());
+
+            names.AddRange((await fpso)
+                .Select(item => item.Name)
+                .ToList());
+
+
+            datas = names.Distinct()
+                .Select(name => new NameFilter(name))
+                .ToList();
         }
         private async Task OnChangeAsync(object value)
         {
