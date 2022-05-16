7aadd7b550c742ce2efe21871768c102e81a5d45
Jianyi Lim <jianyi.lim@appspace.com>
2022-05-16 22:36:07 +0800

    Communication system.

---
+++ src/JXNippon.CentralizedDatabaseSystem/Pages/CommunicationSystem.razor.cs
@@ -0,0 +1,21 @@
+﻿using JXNippon.CentralizedDatabaseSystem.Models;
+using JXNippon.CentralizedDatabaseSystem.Shared;
+using Radzen;
+
+namespace JXNippon.CentralizedDatabaseSystem.Pages
+{
+    public partial class CommunicationSystem
+    {
+        private CommunicationSystemDataGrid dataGrid;
+        private DateFilterPanel filterPanel;
+        private async Task LoadDataAsync(LoadDataArgs args)
+        {
+            dataGrid.CommonFilter = filterPanel.CommonFilter;
+        }
+
+        private async Task OnChangeAsync(CommonFilter commonFilter)
+        {
+            await dataGrid.ReloadAsync();
+        }
+    }
+}
