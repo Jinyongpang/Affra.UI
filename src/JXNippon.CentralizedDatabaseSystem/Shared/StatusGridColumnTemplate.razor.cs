3053347f740603b640883aafdcc1755b38ba820c
Jianyi Lim <jianyi.lim@appspace.com>
2022-05-17 19:09:36 +0800

    Implement Status template component.

---
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/StatusGridColumnTemplate.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace JXNippon.CentralizedDatabaseSystem.Shared
+{
+    public partial class StatusGridColumnTemplate
+    {
+        [Parameter]
+        public string Status { get; set; }
+    }
+}
