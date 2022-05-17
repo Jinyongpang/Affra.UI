65cb2ba1ce41b384a71ae6493a3522f6a4877f87
Jianyi Lim <jianyi.lim@appspace.com>
2022-05-17 21:46:13 +0800

    Add GenericDataGrid.

---
+++ src/JXNippon.CentralizedDatabaseSystem/Pages/Debug.razor.cs
@@ -0,0 +1,20 @@
+﻿using CentralizedDatabaseSystemODataService.Affra.Service.CentralizedDatabaseSystem.Domain.Logistics;
+using JXNippon.CentralizedDatabaseSystem.Models;
+using JXNippon.CentralizedDatabaseSystem.Shared;
+using Radzen;
+
+namespace JXNippon.CentralizedDatabaseSystem.Pages
+{
+    public partial class Debug
+    {
+        private GenericDataGrid<DailyLogistic> dataGrid;
+        private async Task LoadDataAsync(LoadDataArgs args)
+        {
+        }
+
+        private async Task OnChangeAsync(CommonFilter commonFilter)
+        {
+            await dataGrid.ReloadAsync();
+        }
+    }
+}
---
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/GenericDataGrid.razor.cs
@@ -0,0 +1,65 @@
+﻿using Affra.Core.Domain.Services;
+using Affra.Core.Infrastructure.OData.Extensions;
+using JXNippon.CentralizedDatabaseSystem.Domain.CentralizedDatabaseSystemServices;
+using JXNippon.CentralizedDatabaseSystem.Domain.Extensions;
+using JXNippon.CentralizedDatabaseSystem.Models;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+using Radzen.Blazor;
+
+namespace JXNippon.CentralizedDatabaseSystem.Shared
+{
+    public partial class GenericDataGrid<TItem>
+        where TItem : class
+    {
+        private RadzenDataGrid<TItem> grid;
+        private IEnumerable<TItem> items;
+        private bool isLoading = false;
+
+        [Parameter] public EventCallback<LoadDataArgs> LoadData { get; set; }
+        [Parameter] public bool ShowRefreshButton { get; set; }
+        [Parameter] public bool PagerAlwaysVisible { get; set; }
+        [Parameter] public bool ShowDateColumn { get; set; }
+        [Inject] private IServiceProvider ServiceProvider { get; set; }
+        [Inject] private NotificationService NotificationService { get; set; }
+        public CommonFilter CommonFilter { get; set; }
+        public int Count { get; set; }
+
+        public async Task ReloadAsync()
+        {
+            await grid.FirstPage(true);
+        }
+
+        private async Task LoadDataAsync(LoadDataArgs args)
+        {
+            isLoading = true;
+            await LoadData.InvokeAsync();
+            using var serviceScope = ServiceProvider.CreateScope();
+            var service = this.GetGenericService(serviceScope);
+            var query = service.Get();
+            var response = await query
+                .AppendQuery(args.Filter, args.Skip, args.Top, args.OrderBy)
+                .ToQueryOperationResponseAsync<TItem>();
+
+            Count = (int)response.Count;
+            items = response.ToList();
+            isLoading = false;
+        }
+
+        private void HandleException(Exception ex)
+        {
+            NotificationService.Notify(new()
+            {
+                Summary = "Error",
+                Detail = ex.InnerException?.ToString(),
+                Severity = NotificationSeverity.Error,
+                Duration = 120000,
+            });
+        }
+
+        private IGenericService<TItem> GetGenericService(IServiceScope serviceScope)
+        {
+            return serviceScope.ServiceProvider.GetRequiredService<IUnitGenericService<TItem, ICentralizedDatabaseSystemUnitOfWork>>();
+        }
+    }
+}
