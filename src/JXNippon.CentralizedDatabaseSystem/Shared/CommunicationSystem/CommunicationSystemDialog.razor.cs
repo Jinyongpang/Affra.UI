e0b0231cdc27dc322280051c36a6bdd99d4520b8
Eng JunYan <engjunyan@gmail.com>
2022-05-26 22:33:07 +0800

    Add Dialog for Communication system

---
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/CommunicationSystem/CommunicationSystemDialog.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+using CommunicationSystems = CentralizedDatabaseSystemODataService.Affra.Service.CentralizedDatabaseSystem.Domain.CommunicationSystems;
+
+namespace JXNippon.CentralizedDatabaseSystem.Shared.CommunicationSystem
+{
+    public partial class CommunicationSystemDialog
+    {
+        [Parameter] public CommunicationSystems.DailyCommunicationSystem Item { get; set; }
+        [Parameter] public int MenuAction { get; set; }
+        [Inject] private DialogService DialogService { get; set; }
+
+        private bool isViewing { get => MenuAction == 3; }
+
+        protected Task SubmitAsync(CommunicationSystems.DailyCommunicationSystem arg)
+        {
+            DialogService.Close(true);
+            return Task.CompletedTask;
+        }
+
+        private void Cancel()
+        {
+            DialogService.Close(false);
+        }
+    }
+}
