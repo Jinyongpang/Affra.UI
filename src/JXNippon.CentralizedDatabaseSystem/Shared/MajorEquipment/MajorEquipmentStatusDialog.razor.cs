77629362c1ea2764682f820c80ed76b221a75350
Eng JunYan <engjunyan@gmail.com>
2022-05-29 12:10:04 +0800

    Create dialog for Major equipment status

---
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/MajorEquipment/MajorEquipmentStatusDialog.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+using MajorEquipmentStatuses = CentralizedDatabaseSystemODataService.Affra.Service.CentralizedDatabaseSystem.Domain.MajorEquipmentStatuses;
+
+namespace JXNippon.CentralizedDatabaseSystem.Shared.MajorEquipment
+{
+    public partial class MajorEquipmentStatusDialog
+    {
+        [Parameter] public MajorEquipmentStatuses.DailyMajorEquipmentStatus Item { get; set; }
+        [Parameter] public int MenuAction { get; set; }
+        [Inject] private DialogService DialogService { get; set; }
+
+        private bool isViewing { get => MenuAction == 3; }
+
+        protected Task SubmitAsync(MajorEquipmentStatuses.DailyMajorEquipmentStatus arg)
+        {
+            DialogService.Close(true);
+            return Task.CompletedTask;
+        }
+
+        private void Cancel()
+        {
+            DialogService.Close(false);
+        }
+    }
+}
