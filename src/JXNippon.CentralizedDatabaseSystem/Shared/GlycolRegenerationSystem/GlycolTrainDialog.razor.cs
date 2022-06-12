a924b12779e59650c4c0113409f4d73617c85b0b
Eng JunYan <engjunyan@gmail.com>
2022-06-12 21:59:45 +0800

    Create dialog for glycol regeneration system

---
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/GlycolRegenerationSystem/GlycolTrainDialog.razor.cs
@@ -0,0 +1,42 @@
+﻿using Affra.Core.Domain.Services;
+using Affra.Core.Infrastructure.OData.Extensions;
+using CentralizedDatabaseSystemODataService.Affra.Service.CentralizedDatabaseSystem.Domain.GlycolRegenerationSystems;
+using JXNippon.CentralizedDatabaseSystem.Domain.CentralizedDatabaseSystemServices;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace JXNippon.CentralizedDatabaseSystem.Shared.GlycolRegenerationSystem
+{
+    public partial class GlycolTrainDialog
+    {
+        private IEnumerable<GlycolTrain> datas;
+        [Parameter] public DailyGlycolTrain Item { get; set; }
+        [Parameter] public int MenuAction { get; set; }
+        [Inject] private IServiceProvider ServiceProvider { get; set; }
+        [Inject] private DialogService DialogService { get; set; }
+
+        private bool isViewing { get => MenuAction == 3; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (!isViewing)
+            {
+                using var serviceScope = ServiceProvider.CreateScope();
+                datas = (await serviceScope.ServiceProvider.GetRequiredService<IUnitGenericService<GlycolTrain, ICentralizedDatabaseSystemUnitOfWork>>()
+                    .Get()
+                    .ToQueryOperationResponseAsync<GlycolTrain>()).ToList();
+            }
+        }
+
+        protected Task SubmitAsync(DailyGlycolTrain arg)
+        {
+            DialogService.Close(true);
+            return Task.CompletedTask;
+        }
+
+        private void Cancel()
+        {
+            DialogService.Close(false);
+        }
+    }
+}
