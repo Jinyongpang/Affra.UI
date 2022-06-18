2b5e0c15c80077733d6dcea8eab9fc3d3af24937
Eng JunYan <engjunyan@gmail.com>
2022-06-18 12:28:44 +0800

    Create dialog for VendorActivity

---
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/VendorActivities/VendorActivityDialog.razor.cs
@@ -0,0 +1,42 @@
+﻿using Affra.Core.Domain.Services;
+using Affra.Core.Infrastructure.OData.Extensions;
+using CentralizedDatabaseSystemODataService.Affra.Service.CentralizedDatabaseSystem.Domain.Vendors;
+using JXNippon.CentralizedDatabaseSystem.Domain.CentralizedDatabaseSystemServices;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace JXNippon.CentralizedDatabaseSystem.Shared.VendorActivities
+{
+    public partial class VendorActivityDialog
+    {
+        private IEnumerable<Vendor> datas;
+        [Parameter] public DailyVendorActivity Item { get; set; }
+        [Parameter] public int MenuAction { get; set; }
+        [Inject] private IServiceProvider ServiceProvider { get; set; }
+        [Inject] private DialogService DialogService { get; set; }
+
+        private bool isViewing { get => MenuAction == 3; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (!isViewing)
+            {
+                using var serviceScope = ServiceProvider.CreateScope();
+                datas = (await serviceScope.ServiceProvider.GetRequiredService<IUnitGenericService<Vendor, ICentralizedDatabaseSystemUnitOfWork>>()
+                    .Get()
+                    .ToQueryOperationResponseAsync<Vendor>()).ToList();
+            }
+        }
+
+        protected Task SubmitAsync(DailyVendorActivity arg)
+        {
+            DialogService.Close(true);
+            return Task.CompletedTask;
+        }
+
+        private void Cancel()
+        {
+            DialogService.Close(false);
+        }
+    }
+}
