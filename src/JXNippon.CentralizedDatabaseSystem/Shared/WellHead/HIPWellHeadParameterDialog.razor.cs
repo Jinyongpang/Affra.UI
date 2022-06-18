aaa58362f46984135a1397e1b5733fa7b9ce55b8
Eng JunYan <engjunyan@gmail.com>
2022-06-18 11:28:20 +0800

    Create dialog for WellHead

---
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/WellHead/HIPWellHeadParameterDialog.razor.cs
@@ -0,0 +1,42 @@
+﻿using Affra.Core.Domain.Services;
+using Affra.Core.Infrastructure.OData.Extensions;
+using CentralizedDatabaseSystemODataService.Affra.Service.CentralizedDatabaseSystem.Domain.WellHeads;
+using JXNippon.CentralizedDatabaseSystem.Domain.CentralizedDatabaseSystemServices;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace JXNippon.CentralizedDatabaseSystem.Shared.WellHead
+{
+    public partial class HIPWellHeadParameterDialog
+    {
+        private IEnumerable<HIPWellHead> datas;
+        [Parameter] public DailyHIPWellHeadParameter Item { get; set; }
+        [Parameter] public int MenuAction { get; set; }
+        [Inject] private IServiceProvider ServiceProvider { get; set; }
+        [Inject] private DialogService DialogService { get; set; }
+
+        private bool isViewing { get => MenuAction == 3; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (!isViewing)
+            {
+                using var serviceScope = ServiceProvider.CreateScope();
+                datas = (await serviceScope.ServiceProvider.GetRequiredService<IUnitGenericService<HIPWellHead, ICentralizedDatabaseSystemUnitOfWork>>()
+                    .Get()
+                    .ToQueryOperationResponseAsync<HIPWellHead>()).ToList();
+            }
+        }
+
+        protected Task SubmitAsync(DailyHIPWellHeadParameter arg)
+        {
+            DialogService.Close(true);
+            return Task.CompletedTask;
+        }
+
+        private void Cancel()
+        {
+            DialogService.Close(false);
+        }
+    }
+}
