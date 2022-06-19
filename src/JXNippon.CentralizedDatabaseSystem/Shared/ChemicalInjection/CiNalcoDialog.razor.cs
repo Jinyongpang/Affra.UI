f74e2521a2081d647f6db104cbe8338bf3dc91a4
Eng JunYan <engjunyan@gmail.com>
2022-06-19 14:38:53 +0800

    Create dialog for chemical injection

---
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/ChemicalInjection/CiNalcoDialog.razor.cs
@@ -0,0 +1,26 @@
+﻿using CentralizedDatabaseSystemODataService.Affra.Service.CentralizedDatabaseSystem.Domain.ChemicalInjections;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace JXNippon.CentralizedDatabaseSystem.Shared.ChemicalInjection
+{
+    public partial class CiNalcoDialog
+    {
+        [Parameter] public DailyCiNalco Item { get; set; }
+        [Parameter] public int MenuAction { get; set; }
+        [Inject] private DialogService DialogService { get; set; }
+
+        private bool isViewing { get => MenuAction == 3; }
+
+        protected Task SubmitAsync(DailyCiNalco arg)
+        {
+            DialogService.Close(true);
+            return Task.CompletedTask;
+        }
+
+        private void Cancel()
+        {
+            DialogService.Close(false);
+        }
+    }
+}
