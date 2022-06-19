b9682c181aaa684e2b9a685c14832d98942579b1
Eng JunYan <engjunyan@gmail.com>
2022-06-19 20:05:04 +0800

    Create dialog for HSE

---
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/HealthSafetyAndEnvironment/HealthSafetyEnvironmentDialog.razor.cs
@@ -0,0 +1,26 @@
+﻿using CentralizedDatabaseSystemODataService.Affra.Service.CentralizedDatabaseSystem.Domain.HealthSafetyEnvironments;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace JXNippon.CentralizedDatabaseSystem.Shared.HealthSafetyAndEnvironment
+{
+    public partial class HealthSafetyEnvironmentDialog
+    {
+        [Parameter] public DailyHealthSafetyEnvironment Item { get; set; }
+        [Parameter] public int MenuAction { get; set; }
+        [Inject] private DialogService DialogService { get; set; }
+
+        private bool isViewing { get => MenuAction == 3; }
+
+        protected Task SubmitAsync(DailyHealthSafetyEnvironment arg)
+        {
+            DialogService.Close(true);
+            return Task.CompletedTask;
+        }
+
+        private void Cancel()
+        {
+            DialogService.Close(false);
+        }
+    }
+}
---
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/HealthSafetyAndEnvironment/LongTermOverridesInhibitsOnAlarmTripDialog.razor.cs
@@ -0,0 +1,26 @@
+﻿using CentralizedDatabaseSystemODataService.Affra.Service.CentralizedDatabaseSystem.Domain.HealthSafetyEnvironments;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace JXNippon.CentralizedDatabaseSystem.Shared.HealthSafetyAndEnvironment
+{
+    public partial class LongTermOverridesInhibitsOnAlarmTripDialog
+    {
+        [Parameter] public DailyLongTermOverridesInhibitsOnAlarmTrip Item { get; set; }
+        [Parameter] public int MenuAction { get; set; }
+        [Inject] private DialogService DialogService { get; set; }
+
+        private bool isViewing { get => MenuAction == 3; }
+
+        protected Task SubmitAsync(DailyLongTermOverridesInhibitsOnAlarmTrip arg)
+        {
+            DialogService.Close(true);
+            return Task.CompletedTask;
+        }
+
+        private void Cancel()
+        {
+            DialogService.Close(false);
+        }
+    }
+}
