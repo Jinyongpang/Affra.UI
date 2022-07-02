b656f2a8f47c4599c561ccb060bfed33caf9fbbe
Jianyi Lim <jianyi.lim@appspace.com>
2022-07-02 14:32:36 +0800

    Bug fix.

--- src/JXNippon.CentralizedDatabaseSystem/Pages/Operation.razor.cs
+++ src/JXNippon.CentralizedDatabaseSystem/Pages/Operation.razor.cs
@@ -3,28 +3,39 @@ using Microsoft.AspNetCore.Components;
 
 namespace JXNippon.CentralizedDatabaseSystem.Pages
 {
-    public partial class Operation
+    public partial class Operation : IDisposable
     {
-        private string activeKey = string.Empty;
         private Tabs tabs;
+        private bool isDisposed = false;
+
+        [Parameter]
+        public string activeKey { get; set; }
 
         [Inject] private NavigationManager NavigationManager { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            this.activeKey = NavigationManager.ToBaseRelativePath(NavigationManager.Uri).ToLowerInvariant();
             NavigationManager.LocationChanged += NavigationManager_LocationChanged;
         }
 
         private void NavigationManager_LocationChanged(object? sender, Microsoft.AspNetCore.Components.Routing.LocationChangedEventArgs e)
-        {           
-            this.activeKey = NavigationManager.ToBaseRelativePath(NavigationManager.Uri).ToLowerInvariant();
+        {
             tabs.ActiveKey = activeKey;
         }
 
         private void OnActiveKeyChanged(string activeKey)
         {
-            NavigationManager.NavigateTo(activeKey);
+            NavigationManager.NavigateTo($"operation/{activeKey}");
+        }
+
+        void IDisposable.Dispose()
+        {
+            if (!isDisposed)
+            {
+                tabs.Dispose();
+                NavigationManager.LocationChanged -= NavigationManager_LocationChanged;
+                isDisposed = true;
+            }
         }
     }
 }
---
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/LoadingComponent.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace JXNippon.CentralizedDatabaseSystem.Shared
+{
+    public partial class LoadingComponent
+    {
+
+    }
+}
