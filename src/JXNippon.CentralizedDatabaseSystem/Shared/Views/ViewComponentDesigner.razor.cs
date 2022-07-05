9fbbe345ed8ede03de01fb40d3fefe107fe8507a
Jianyi Lim <jianyi.lim@appspace.com>
2022-07-06 00:33:44 +0800

    Improve view designer.

--- src/JXNippon.CentralizedDatabaseSystem/Shared/Views/ViewComponentDesigner.razor.cs
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/Views/ViewComponentDesigner.razor.cs
@@ -1,4 +1,9 @@
-﻿using JXNippon.CentralizedDatabaseSystem.Domain.Views;
+﻿using Affra.Core.Domain.Services;
+using JXNippon.CentralizedDatabaseSystem.Domain.Charts;
+using JXNippon.CentralizedDatabaseSystem.Domain.Grids;
+using JXNippon.CentralizedDatabaseSystem.Domain.Views;
+using JXNippon.CentralizedDatabaseSystem.Notifications;
+using JXNippon.CentralizedDatabaseSystem.Shared.Constants;
 using Microsoft.AspNetCore.Components;
 using Radzen;
 using Radzen.Blazor;
@@ -9,6 +14,8 @@ namespace JXNippon.CentralizedDatabaseSystem.Shared.Views
     public partial class ViewComponentDesigner
     {
         [Inject] private IServiceProvider ServiceProvider { get; set; }
+        [Inject] private AffraNotificationService AffraNotificationService { get; set; }
+        [Inject] private DialogService DialogService { get; set; }
 
         private IEnumerable<View> views = new List<View>();
         private RadzenDropDown<View> radzenDropDown;
@@ -33,10 +40,16 @@ namespace JXNippon.CentralizedDatabaseSystem.Shared.Views
         private async Task OnChangeAsync(object value)
         {
             view = value as View;
+            await this.ReloadAsync();
+        }
+
+        public async Task ReloadAsync(View value = null)
+        {
+            view = value ?? this.view;
             using var serviceScope = ServiceProvider.CreateScope();
             IViewService viewService = serviceScope.ServiceProvider.GetService<IViewService>();
             await viewService.GetViewDetailAsync(view);
-            await Task.WhenAll(columnDataGrid.ReloadAsync(), rowDataGrid.ReloadAsync());
+            await viewComponent.ReloadAsync();
         }
 
         private async Task LoadDataAsync(LoadDataArgs args)
@@ -53,6 +66,73 @@ namespace JXNippon.CentralizedDatabaseSystem.Shared.Views
                 view = radzenDropDown.SelectedItem as View;
                 await viewService.GetViewDetailAsync(view);
                 await viewComponent.ReloadAsync();
+            }
+        }
+
+        private async Task AddRowAsync()
+        {
+            using var serviceScope = ServiceProvider.CreateScope();
+            var genericService = serviceScope.ServiceProvider.GetRequiredService<IUnitGenericService<Row, IViewUnitOfWork>>();
+            var seq = view.Rows.Count > 0
+                ? view.Rows.Max(row => row.Sequence) + 1
+                : 0;
+            var row = new Row() { ViewName = view.Name, Sequence = seq };
+            await genericService.InsertAsync(row);
+            AffraNotificationService.NotifyItemCreated();
+            view.Rows.Add(row);
+            StateHasChanged();
+        }
+
+        private IGenericService<T> GetGenericService<T>(IServiceScope serviceScope)
+            where T : class
+        {
+            return serviceScope.ServiceProvider.GetRequiredService<IUnitGenericService<T, IViewUnitOfWork>>();
+        }
+
+        private async Task ShowDialogAsync(Column data, string title)
+        {
+            dynamic? response = null;
+            if (data.ComponentType == nameof(Chart))
+            {
+                response = await DialogService.OpenAsync<ChartDialog>(title,
+                   new Dictionary<string, object>() { { "Column", data }, { "View", view } },
+                   Constant.DialogOptions);
+            }
+            else if (data.ComponentType == nameof(Grid))
+            {
+                response = await DialogService.OpenAsync<GridDialog>(title,
+                   new Dictionary<string, object>() { { "Column", data }, { "View", view } },
+                   Constant.DialogOptions);
+            }
+
+
+            if (response == true)
+            {
+                try
+                {
+                    using var serviceScope = ServiceProvider.CreateScope();
+                    var service = this.GetGenericService<Column>(serviceScope);
+
+                    if (data.Id > 0)
+                    {
+                        await service.UpdateAsync(data, data.Id);
+                        AffraNotificationService.NotifyItemUpdated();
+                    }
+                    else
+                    {
+                        await service.InsertAsync(data);
+                        AffraNotificationService.NotifyItemCreated();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    AffraNotificationService.NotifyException(ex);
+                }
+                finally
+                {
+                    await this.ReloadAsync();
+                }
             }
         }
     }
