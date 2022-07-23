c44176fe2d2e2409cc1d289475eb96d2d1be2cf7
Jianyi Lim <jianyi.lim@appspace.com>
2022-07-23 12:42:18 +0800

    Pull MOC.

---
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/ManagementOfChange/ManagementOfChangeCardTemplate.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+using JXNippon.CentralizedDatabaseSystem.Shared.Constants;
+
+namespace JXNippon.CentralizedDatabaseSystem.Shared.ManagementOfChange
+{
+    public partial class ManagementOfChangeCardTemplate
+    {
+        [Parameter] public string RecordCode { get; set; }
+        [Parameter] public string Description { get; set; }
+        [Inject] private DialogService DialogService { get; set; }
+        private async Task ShowDialogAsync(string recordCode, string description, string title)
+        {
+            await DialogService.OpenAsync<MOCFormTemplate>(title,
+                    new Dictionary<string, object>() { { "RecordCode", recordCode }, { "Description", description } },
+                    Constant.MOHDialogOptions);
+        }
+    }
+}
