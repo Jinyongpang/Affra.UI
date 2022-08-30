4ef466a9d80b150adfe08efddf3efb61d566789b
Jianyi Lim <jianyi.lim@appspace.com>
2022-08-31 00:43:02 +0800

    Dialog style.

--- src/JXNippon.CentralizedDatabaseSystem/Shared/Constants/Constant.cs
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/Constants/Constant.cs
@@ -5,10 +5,10 @@ namespace JXNippon.CentralizedDatabaseSystem.Shared.Constants
     public static class Constant
     {
         public const string DialogStyle = "min-height:auto;max-width:100%; width:auto; min-width:30vw;";
-        public const string MOHDialogStyle = "height:100vh;width:100vw;padding:0px";
+        public const string MOHDialogStyle = "height:100vh;width:100vw;padding:0px; border-radius:0 !important;";
 
         public readonly static DialogOptions DialogOptions = new DialogOptions() { Style = DialogStyle, Resizable = true, Draggable = true, CloseDialogOnOverlayClick = true };
-        public readonly static DialogOptions MOHDialogOptions = new DialogOptions() { Style = MOHDialogStyle, Resizable = true, Draggable = false, CloseDialogOnOverlayClick = true };
+        public readonly static DialogOptions MOHDialogOptions = new DialogOptions() { Style = MOHDialogStyle, Resizable = false, Draggable = false, ShowClose = false, ShowTitle = false, CloseDialogOnOverlayClick = true };
 
         public readonly static string[] Colors = new string[]
         {
