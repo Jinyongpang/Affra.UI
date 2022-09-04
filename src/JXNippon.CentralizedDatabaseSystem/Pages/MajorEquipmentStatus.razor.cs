9d7a51ef6007917379dec34a36d91ec490f020e8
Eng JunYan <engjunyan@gmail.com>
2022-09-04 13:28:26 +0800

    Update CDS changes

--- src/JXNippon.CentralizedDatabaseSystem/Pages/MajorEquipmentStatus.razor.cs
+++ src/JXNippon.CentralizedDatabaseSystem/Pages/MajorEquipmentStatus.razor.cs
@@ -1,4 +1,5 @@
 ﻿using JXNippon.CentralizedDatabaseSystem.Models;
+using JXNippon.CentralizedDatabaseSystem.Shared;
 using JXNippon.CentralizedDatabaseSystem.Shared.MajorEquipment;
 using Radzen;
 
@@ -7,15 +8,18 @@ namespace JXNippon.CentralizedDatabaseSystem.Pages
     public partial class MajorEquipmentStatus
     {
         private MajorEquipmentStatusDataGrid dataGrid;
+        private DieselDataGrid dieselDataGrid;
         private MajorEquipmentStatusFilterPanel filterPanel;
+        private DateFilterPanel dateFilterPanel;
         private async Task LoadDataAsync(LoadDataArgs args)
         {
             dataGrid.CommonFilter = filterPanel.CommonFilter;
+            dieselDataGrid.DailyDataGrid.CommonFilter = dateFilterPanel.CommonFilter;
         }
 
         private async Task OnChangeAsync(CommonFilter commonFilter)
         {
-            await dataGrid.ReloadAsync();
+            await Task.WhenAll(dataGrid.ReloadAsync(), dieselDataGrid.DailyDataGrid.ReloadAsync());
         }
     }
 }
---
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/MajorEquipment/DieselDataGrid.razor.cs
@@ -0,0 +1,18 @@
+﻿using CentralizedDatabaseSystemODataService.Affra.Service.CentralizedDatabaseSystem.Domain.MajorEquipmentStatuses;
+using JXNippon.CentralizedDatabaseSystem.Models;
+using JXNippon.CentralizedDatabaseSystem.Shared.Commons;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace JXNippon.CentralizedDatabaseSystem.Shared.MajorEquipment
+{
+    public partial class DieselDataGrid
+    {
+        [Parameter] public EventCallback<LoadDataArgs> LoadData { get; set; }
+        [Parameter] public bool ShowRefreshButton { get; set; }
+        [Parameter] public bool PagerAlwaysVisible { get; set; }
+        [Parameter] public bool ShowDateColumn { get; set; }
+
+        public DailyDataGrid<DailyDiesel, DieselDialog> DailyDataGrid { get; set; }
+    }
+}
