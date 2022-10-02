9d7c5d27b5593f2f819d11586440ca0337c9ab7a
Jianyi Lim <jianyi.lim@appspace.com>
2022-10-02 19:25:00 +0800

    Render first row first.

---
+++ src/JXNippon.CentralizedDatabaseSystem.Domain/Connected Services/ViewODataService/View.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.OData.Client;
+
+namespace ViewODataService.Affra.Service.View.Domain.Views
+{
+    public partial class View
+    {
+        [IgnoreClientProperty]
+        public ICollection<Row> RowsStartFromSecond 
+        { 
+            get 
+            {   List<Row> list = new List<Row>();
+                for (int i = 1; i < Rows.Count; i++)
+                { 
+                    list.Add(this.Rows[i]);
+                }
+                return list;
+            } 
+        }
+
+        [IgnoreClientProperty]
+        public ICollection<Row> RowsFirst
+        {
+            get
+            {
+                List<Row> list = new List<Row>();
+                for (int i = 0; i < Rows.Count && i < 1; i++)
+                {
+                    list.Add(this.Rows[i]);
+                }
+                return list;
+            }
+        }
+    }
+}
