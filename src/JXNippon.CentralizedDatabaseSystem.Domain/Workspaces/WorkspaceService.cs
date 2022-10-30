db5a6146becb1ecc1ae75bc1a8352546fad50d88
Jianyi Lim <jianyi.lim@appspace.com>
2022-10-30 19:56:24 +0800

    Download and upload file.

---
+++ src/JXNippon.CentralizedDatabaseSystem.Domain/Workspaces/WorkspaceService.cs
@@ -0,0 +1,29 @@
+﻿using JXNippon.CentralizedDatabaseSystem.Domain.WorkspaceAPI;
+
+namespace JXNippon.CentralizedDatabaseSystem.Domain.Workspaces
+{
+    public class WorkspaceService : IWorkspaceService
+    {
+        private readonly IWorkspaceAPIClient workspaceAPIClient;
+        public WorkspaceService(IWorkspaceAPIClient workspaceAPIClient)
+        {
+            this.workspaceAPIClient = workspaceAPIClient;
+        }
+
+        public Task<WorkspaceFile> UploadAsync(string workspace, FileParameter file, CancellationToken cancellationToken = default)
+        {
+            return workspaceAPIClient.WorkspaceFilePostAsync(workspace, file, cancellationToken);
+        }
+
+        public Task<Guid> GetIdAsync(string workspaceName, string fileName, CancellationToken cancellationToken = default)
+        {
+            return workspaceAPIClient.WorkspaceFileGetAsync(workspaceName, fileName, cancellationToken);
+        }
+
+        public Task<FileResponse> DownloadAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            return workspaceAPIClient.WorkspaceFileGetAsync(id, cancellationToken);
+        }
+
+    }
+}
