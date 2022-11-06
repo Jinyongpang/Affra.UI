95f514f0f20a0dd73173e63b0511a5c65610dc33
Jianyi Lim <jianyi.lim@appspace.com>
2022-11-06 17:10:07 +0800

    Calculate summary.

--- src/JXNippon.CentralizedDatabaseSystem/Shared/OIMSummary/FPSOHelangSummaryDataGrid.razor.cs
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/OIMSummary/FPSOHelangSummaryDataGrid.razor.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using CentralizedDatabaseSystemODataService.Affra.Service.CentralizedDatabaseSystem.Domain.CombinedDailyReports;
 using CentralizedDatabaseSystemODataService.Affra.Service.CentralizedDatabaseSystem.Domain.OIMSummaries;
 using JXNippon.CentralizedDatabaseSystem.Models;
 using JXNippon.CentralizedDatabaseSystem.Shared.Commons;
@@ -10,6 +11,7 @@ namespace JXNippon.CentralizedDatabaseSystem.Shared.OIMSummary
     public partial class FPSOHelangSummaryDataGrid
     {
         [Parameter] public Collection<DailyFPSOHelangSummary> Data { get; set; }
+        [Parameter] public CombinedDailyReport CombinedDailyReport { get; set; }
         [Parameter] public EventCallback<Collection<DailyFPSOHelangSummary>> DataChanged { get; set; }
         [Parameter] public EventCallback<LoadDataArgs> LoadData { get; set; }
         [Parameter] public bool ShowRefreshButton { get; set; }
@@ -34,6 +36,76 @@ namespace JXNippon.CentralizedDatabaseSystem.Shared.OIMSummary
         public Task ReloadAsync()
         {
             return this.DailyDataGrid.ReloadAsync();
+        }
+
+        private Task OnDataChangedAsync(Collection<DailyFPSOHelangSummary> data)
+        {
+            var result = new List<DailyFPSOHelangSummary>();
+            if (this.ReportDate is not null
+                && this.CombinedDailyReport is not null
+                && this.CombinedDailyReport.DailyFPSOHelangProduction is not null
+                && this.CombinedDailyReport.DailyFPSOHelangProduction.FPSOCumulativeGasExport is not null)
+            {
+                result.Add(new()
+                {
+                    Date = this.CombinedDailyReport.Date,
+                    Remark = this.CalculateSummary(),
+                });
+            }
+            foreach (var item in data)
+            {
+                result.Add(item);
+            }
+            data = new Collection<DailyFPSOHelangSummary>(result);
+            return DataChanged.InvokeAsync(data);
+        }
+
+        private string CalculateSummary()
+        {
+            var fpsoProduction = this.CombinedDailyReport.DailyFPSOHelangProduction;
+            if (fpsoProduction.FPSOCumulativeGasExport > 0)
+            {
+                return $"<a>FPSO Helang cumulative Gas Export of "
+                    + $"<a style=\"background-color:#FEFB00;\">{fpsoProduction.FPSOCumulativeGasExport}</a>"
+                    + " MMscf was "
+                    + $"<a style=\"background-color:#02F900;\">{this.GetCondition()}</a>"
+                    + " than GODC "
+                    + $"<a style=\"background-color:#00FDFF;\">{this.CombinedDailyReport.DailyFPSOHelangProduction?.FPSOCumulativeGasExportRemark}</a>"
+                    + " plan of "
+                    + $"<a style=\"background-color:#FF40FF;\">{this.CombinedDailyReport.DailyFPSOHelangProduction?.FPSOGasExportGODCPlan}</a>"
+                    + " MMscf. FPSO daily cumulative Crude Oil into storage tank was "
+                    + $"<a style=\"background-color:#FF2800;\">{this.CombinedDailyReport.DailyFPSOHelangProduction?.FPSOCumulativeCrudeOil}</a>"
+                    + "bbls.</a>";
+            }
+            else if (fpsoProduction.FPSOCumulativeGasExport == 0)
+            {
+                return "FPSO Helang shutdown";
+            }
+            return string.Empty;
+        }
+
+        private string GetCondition()
+        {
+            var fpsoGasExport = this.CombinedDailyReport.DailyFPSOHelangProduction?.FPSOCumulativeGasExport ?? 0;
+            var fpsoGasExportPlan = this.CombinedDailyReport.DailyFPSOHelangProduction?.FPSOGasExportGODCPlan ?? 0;
+            var difference = fpsoGasExport - fpsoGasExportPlan;
+            if (difference < 0 && difference > -1.0m)
+            {
+                return "Slightly lower";
+            }
+            else if (difference < 0)
+            {
+                return "lower";
+            }
+            else if (difference > 0 && difference < 1.0m)
+            {
+                return "Slightly higher";
+            }
+            else if (difference > 0)
+            {
+                return "higher";
+            }
+            return "Equal";
         }
     }
 }
