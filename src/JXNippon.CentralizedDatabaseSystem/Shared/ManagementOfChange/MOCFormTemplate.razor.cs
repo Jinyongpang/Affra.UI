7aa6e473d126fb4c1e5c08110ca62d336c916787
Jianyi Lim <jianyi.lim@appspace.com>
2022-12-12 16:46:19 +0800

    fix error

--- src/JXNippon.CentralizedDatabaseSystem/Shared/ManagementOfChange/MOCFormTemplate.razor.cs
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/ManagementOfChange/MOCFormTemplate.razor.cs
@@ -298,9 +298,6 @@ namespace JXNippon.CentralizedDatabaseSystem.Shared.ManagementOfChange
             return string.Empty;
         }
 
-            return string.Empty;
-        }
-
         private async Task<ICollection<string>> GetLineManagersAndDelegationsAsync()
         {
             try 
