531c4fb3a986c83dffc432cf2399b49b3214ce1c
Duncan Foo <duncan.foo@affra.onmicrosoft.com>
2022-12-22 20:34:02 +0800

    - Added AnR dashboard

--- src/JXNippon.CentralizedDatabaseSystem/Shared/AvailabilityAndReliabilityReport/AvailabilityAndReliabilityDashboardView.razor.cs
+++ src/JXNippon.CentralizedDatabaseSystem/Shared/AvailabilityAndReliabilityReport/AvailabilityAndReliabilityDashboardView.razor.cs
@@ -12,7 +12,7 @@ namespace JXNippon.CentralizedDatabaseSystem.Shared.AvailabilityAndReliabilityRe
     class DataItem
     {
         public string Month { get; set; }
-        public decimal Percentage { get; set; }
+        public double Percentage { get; set; }
     }
     public partial class AvailabilityAndReliabilityDashboardView
     {
@@ -32,10 +32,31 @@ namespace JXNippon.CentralizedDatabaseSystem.Shared.AvailabilityAndReliabilityRe
         private List<int> YearList { get; set; }
 
         private string[] PropertyNames = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "YTD", "YEP", "Q1", "Q2", "Q3", "Q4" };
+
+        private List<DataItem> HIPAvailabilityChartList { get; set; }
+        private List<DataItem> HIPReliabilityChartList { get; set; }
+        private List<DataItem> FPSOAvailabilityChartList { get; set; }
+        private List<DataItem> FPSOReliabilityChartList { get; set; }
+        private List<DataItem> LayangAvailabilityChartList { get; set; }
+        private List<DataItem> LayangReliabilityChartList { get; set; }
+        private List<DataItem> AverageAvailabilityChartList { get; set; }
+        private List<DataItem> AverageReliabilityChartList { get; set; }
+        private List<DataItem> TargetChartList { get; set; }
+        private List<double> HIPAvailabilityPercentageList { get; set; }
+        private List<double> HIPReliabilityPercentageList { get; set; }
+        private List<double> FPSOAvailabilityPercentageList { get; set; }
+        private List<double> FPSOReliabilityPercentageList { get; set; }
+        private List<double> LayangAvailabilityPercentageList { get; set; }
+        private List<double> LayangReliabilityPercentageList { get; set; }
+        private List<double> AverageAvailabilityPercentageList { get; set; }
+        private List<double> AverageReliabilityPercentageList { get; set; }
+        private List<double> TargetPercentageList { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
             await this.LoadYearAsync();
             await this.LoadDataAsync();
+            await this.LoadListAsync();
         }
         private Task OnMenuItemSelectAsync(MenuItem menuItem)
         {
@@ -214,27 +235,131 @@ namespace JXNippon.CentralizedDatabaseSystem.Shared.AvailabilityAndReliabilityRe
 
             StateHasChanged();
         }
+        public async Task LoadListAsync()
+        {
+            if (HIPAvailabilityItems is not null)
+            {
+                HIPAvailabilityChartList = new List<DataItem>();
+                HIPAvailabilityPercentageList = new List<double>();
+
+                CreateListData(HIPAvailabilityItems, HIPAvailabilityChartList, HIPAvailabilityPercentageList);
+            }
+
+            if (HIPReliabilityItems is not null)
+            {
+                HIPReliabilityChartList = new List<DataItem>();
+                HIPReliabilityPercentageList = new List<double>();
+
+                CreateListData(HIPReliabilityItems, HIPReliabilityChartList, HIPReliabilityPercentageList);
+            }
+
+            if (FPSOAvailabilityItems is not null)
+            {
+                FPSOAvailabilityChartList = new List<DataItem>();
+                FPSOAvailabilityPercentageList = new List<double>();
+
+                CreateListData(FPSOAvailabilityItems, FPSOAvailabilityChartList, FPSOAvailabilityPercentageList);
+            }
+
+            if (FPSOReliabilityItems is not null)
+            {
+                FPSOReliabilityChartList = new List<DataItem>();
+                FPSOReliabilityPercentageList = new List<double>();
+
+                CreateListData(FPSOReliabilityItems, FPSOReliabilityChartList, FPSOReliabilityPercentageList);
+            }
+
+
+            if (LayangAvailabilityItems is not null)
+            {
+                LayangAvailabilityChartList = new List<DataItem>();
+                LayangAvailabilityPercentageList = new List<double>();
+
+                CreateListData(LayangAvailabilityItems, LayangAvailabilityChartList, LayangAvailabilityPercentageList);
+            }
+
+            if (LayangReliabilityItems is not null)
+            {
+                LayangReliabilityChartList = new List<DataItem>();
+                LayangReliabilityPercentageList = new List<double>();
+
+                CreateListData(LayangReliabilityItems, LayangReliabilityChartList, LayangReliabilityPercentageList);
+            }
+
+            if (AverageAvailabilityItems is not null)
+            {
+                AverageAvailabilityChartList = new List<DataItem>();
+                AverageAvailabilityPercentageList = new List<double>();
+
+                CreateListData(AverageAvailabilityItems, AverageAvailabilityChartList, AverageAvailabilityPercentageList);
+            }
+
+            if (AverageReliabilityItems is not null)
+            {
+                AverageReliabilityChartList = new List<DataItem>();
+                AverageReliabilityPercentageList = new List<double>();
+
+                CreateListData(AverageReliabilityItems, AverageReliabilityChartList, AverageReliabilityPercentageList);
+            }
+
+            if (TargetItems is not null)
+            {
+                TargetChartList = new List<DataItem>();
+                TargetPercentageList = new List<double>();
+
+                CreateListData(TargetItems, TargetChartList, TargetPercentageList);
+            }
+        }
+        private void CreateListData(object yearlyItem, List<DataItem> chartList, List<double> percentageList)
+        {
+            double percentage = 0.00;
+
+            if (yearlyItem is not null)
+            {
+                percentage = 0.00;
+
+                for (int i = 0; i < PropertyNames.Length; i++)
+                {
+                    percentage = GetYearlyPropertyValue(yearlyItem, PropertyNames[i].ToString());
+
+                    percentageList.Add(percentage);
+
+                    if (i < 12)
+                    {
+                        chartList.Add(new DataItem { Month = PropertyNames[i], Percentage = percentage });
+                    }
+                }
+            }
+        }
         public double GetYearlyPropertyValue(object yearlyItem, string month)
         {
-            Console.WriteLine($"Month : {month}");
-            if (yearlyItem is null)
+            try
             {
-                return 100.00;
-            }
-            Type t = yearlyItem.GetType();
-            PropertyInfo info = t.GetProperty(month);
+                if (yearlyItem is null)
+                {
+                    return 100.00;
+                }
+                Type t = yearlyItem.GetType();
+                PropertyInfo info = t.GetProperty(month);
 
-            if (info is null)
+                if (info is null)
+                {
+                    return 100.00;
+                }
+
+                if (!info.CanRead)
+                {
+                    return 100.00;
+                }
+
+                return Convert.ToDouble(info.GetValue(yearlyItem, null));
+            }
+            catch (Exception ex)
             {
                 return 100.00;
+                this.AffraNotificationService.NotifyException(ex);
             }
 
-            if (!info.CanRead)
-            {
-                return 100.00;
-            }
-            Console.WriteLine($"{month} : {Convert.ToDouble(info.GetValue(yearlyItem, null))}");
-            return Convert.ToDouble(info.GetValue(yearlyItem, null));
         }
     }
 }
